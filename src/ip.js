d22ac4f37199ea3ea54bd13ab6d9eb37296f2bfe
jignesh119 <jignesh3503@gmail.com>
2024-08-28 17:06:02 +0530

    setup cli flags for port, ip address, receive or upload mode; and handle incorrect values and use passed values instead of hardcoded ones

--- src/ip.js
+++ src/ip.js
@@ -100,64 +100,137 @@ const findAvaliablePort = () => {
 };
 
 const main = async () => {
-  let port, address, filePath, mode, debug, cli, message, ssid;
+  let port, address, filePath, mode, debug, cli, ssid;
   const helpText = `
-		cli-share - cli tool to share files btw devices in same network by scanning a qr
+		cli-share - cli tool to share files btw devices in local network without leaving cli
 		${chalk.bold("Usage:")} 
 
     ${chalk.italic("- file sharing: ")} cli-share [option {value}] <path>
+      $ cli-share /path/to/file || /path/to/folder
+      $ cli-share --recieve,-r /path/to/receive/file/to
+      $ cli-share --port,-p PORT
+      $ cli-share --ip,-i IP_ADDRESS
       $ cli-share --help
       $ cli-share --version
 
 		${chalk.bold("Options:")}
+      -r --receive \tenable upload mode, given path's used to store recieved file, defaults to pwd
+      -p --port    \tuse a custom port for server; note: 1024<=PORT<=65535 to avoid conflicting errors
+      -i --ip      \tbind webserver to custom ip, must be accessible from within subnetwork 
       -h --help    \tshow this screen
       -v --version \tshow version
 
 		${chalk.bold("Examples:")}
-      $ cli-share --help
-      $ cli-share --version
+      $ cli-share ./assets.pdf
+      $ cli-share one.txt -p 4000 --ip 192.168.1.10
+      $ cli-share sampleFolder
+      $ cli-share --receive ./Downloads
+      $ cli-share -r
+      $ cli-share -r ./Downloads -p 5000 -i 192.168.200.23
+      $ cli-share -m "I love cli"
 	`;
   getLocalIpsAvailable(async (err, ips) => {
-    cli = meow({
-      importMeta: import.meta,
-      help: helpText,
-      flags: {
-        help: {
-          type: "boolean",
-          default: false,
-          shortFlag: "h",
+    if (err) {
+      console.log(chalk.red(`unable to get network interfaces`));
+      process.exit(1);
+    } else {
+      cli = meow({
+        importMeta: import.meta,
+        help: helpText,
+        flags: {
+          ip: {
+            type: "string",
+            default: getLocalIp(),
+            choices: ips,
+            shortFlag: "i",
+            aliases: ["IP", "iP"],
+            isMultiple: false,
+          },
+          port: {
+            type: "number",
+            default: 1023,
+            shortFlag: "p",
+            aliases: ["Port", "PORT"],
+            isMultiple: false,
+          },
+          help: {
+            type: "boolean",
+            default: false,
+            shortFlag: "h",
+          },
+          version: {
+            type: "boolean",
+            default: false,
+            shortFlag: "v",
+          },
+          receive: {
+            type: "boolean",
+            default: false,
+            shortFlag: "r",
+          },
         },
-        version: {
-          type: "boolean",
-          default: false,
-          shortFlag: "v",
-        },
-      },
-      description: false,
-    });
-    debug = true;
-    mode = null;
-    ssid = getSSID();
-    address = getLocalIp();
-    console.log(
-      `make sure your device is connected to ${chalk.blue.bold(ssid)}`,
-    );
-    if (mode) {
-      startUploadServer({
-        filePath,
-        port: 4000,
-        address,
-        debug: true,
-        mode: mode,
+        description: false,
       });
-    } else
-      startDownloadServer({
-        filePath: "./sample.txt",
-        port: 4000,
-        address,
-        debug: true,
-        mode: null,
-      });
+      debug = true;
+      mode = cli.flags.r || cli.flags.receive;
+      port = cli.flags.port || cli.flags.p || cli.flags.Port || cli.flags.PORT;
+      address = cli.flags.ip || cli.flags.i || cli.flags.IP || cli.flags.iP;
+      filePath = cli.input[0];
+      if (!filePath && mode) {
+        filePath = ".";
+      }
+      if (!fs.existsSync(filePath)) {
+        console.log(chalk.red(`No such file or directory ${filePath}`));
+        if (!mode) console.log(chalk.red(`specify a valid file to send`));
+        if (mode) console.log(chalk.red(`specify a folder to receive to`));
+        console.log(chalk.red(`see cli-share --help for more info`));
+        process.exit(1);
+      }
+      const stats = fs.statSync(path.normalize(path.resolve(filePath)));
+      if (mode && !stats.isDirectory()) {
+        console.log(chalk.red(`${filePath} is not a folder`));
+        console.log(chalk.red(`see cli-share --help for more info`));
+        process.exit(1);
+      }
+      if (
+        mode &&
+        path.basename(path.normalize(path.resolve(filePath))) == "public"
+      ) {
+        console.log(chalk.red.bold(`cannot choose public folder to receive`));
+        process.exit(1);
+      }
+      if (!port || port < 1024) {
+        try {
+          port = await findAvaliablePort();
+        } catch (err) {
+          console.log(`failed to find available port, try again later ${err}`);
+          process.exit(1);
+        }
+      }
+      if (!address) {
+        address = getLocalIp();
+      }
+      ssid = getSSID();
+      console.log(
+        `make sure your device is connected to ${chalk.blue.bold(ssid)}`,
+      );
+      if (mode) {
+        startUploadServer({
+          filePath,
+          port,
+          address,
+          debug,
+          mode,
+        });
+      } else
+        startDownloadServer({
+          filePath,
+          port,
+          address,
+          debug,
+          mode: null,
+        });
+    }
   });
 };
 main();
